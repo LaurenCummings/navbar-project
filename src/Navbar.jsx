9d7f81bb505d0664c87de0e5264170da646e58e5
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-05-22 23:02:51 -0400

    downloaded data file and logo from project website and imported

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from 'react';
 import { FaBars, FaTwitter } from 'react-icons/fa';
+import { links, social } from './data';
+import logo from './logo.svg'
 
 function Navbar() {
     return (
