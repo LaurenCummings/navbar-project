11df4fec6193c77d686fb2f5dada3bbe9193c209
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-05-24 22:53:04 -0400

    Added toggle for showLinks on hamburger click

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,31 +1,37 @@
 import './Navbar.css';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { FaBars } from 'react-icons/fa';
 import { FaXTwitter } from 'react-icons/fa6'
 import { links, social } from './data.jsx';
 import logo from './logo.svg'
 
 function Navbar() {
+    const [showLinks, setShowLinks] = useState(false);
+
     return (
         <nav>
             <div className="nav-center">
                 <div className="nav-header">
                     <img src={logo} alt="logo" />
-                    <button className="nav-toggle">
+                    <button className="nav-toggle" onClick={() => setShowLinks(!showLinks)}>
                         <FaBars />
                     </button>
                 </div>
-                <div className="links-container show-container">
-                    <ul className="links">
-                        {links.map((link) => {
-                            const { id, url, text } = link;
-                            return (
-                                <li key={id}>
-                                    <a href={url}>{text}</a>
-                                </li>
-                            )
-                        })}
-                    </ul>
+                {showLinks && (
+                    <div className="links-container show-container">
+                        <ul className="links">
+                            {links.map((link) => {
+                                const { id, url, text } = link;
+                                return (
+                                    <li key={id}>
+                                        <a href={url}>{text}</a>
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                    </div>
+                )}
+                <div>
                     <ul className="social-icons">
                         {social.map((socialIcon) => {
                             const { id, url, icon } = socialIcon;
